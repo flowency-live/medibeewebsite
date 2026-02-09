e5adb4a1afd73fcbc750626f866d880e52f689a6
BandFlow25 <jason@flowency.co.uk>
2026-02-09 10:22:59 +0000

    fix: show full logo on mobile and update favicons

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -34,7 +34,7 @@ export function Header() {
               alt="Medibee"
               width={140}
               height={40}
-              className="h-8 w-auto hidden lg:block"
+              className="h-6 md:h-8 w-auto"
             />
           </Link>
 
